27131b7e89ce73c07c5664a9ca8777d174c341e9
Sandi Eng <sandi1_eng@yahoo.com>
2018-05-14 10:10:02 +1000

    .

--- BridgePattern/Abstraction/Manuscript.cs
+++ BridgePattern/Abstraction/Manuscript.cs
@@ -1,9 +1,7 @@
 ﻿namespace BridgePattern
 {
     public abstract class Manuscript
-    {
-        // Decouple an abstraction from its implementation so that the two can vary independently.
-        // Publish interface in an inheritance hierarchy, and bury implementation in its own inheritance hierarchy.
+    {      
         public string Title { get; set; }
         public string TotalPages { get; set; }
         public string YearPublished { get; set; }
--- BridgePattern/RefinedAbstraction/Book.cs
+++ BridgePattern/RefinedAbstraction/Book.cs
@@ -6,11 +6,15 @@ namespace BridgePattern
     {
         private IPrintFormatter _formatter;
 
+
+        // Each print method has its own implementation
         public Book(IPrintFormatter formatter)
         {
             _formatter = formatter;
         }
 
+        // Decouple an abstraction from its implementation so that the two can vary independently.
+        // Publish interface in an inheritance hierarchy, and bury implementation in its own inheritance hierarchy.
         public override void Print()
         {
             Console.WriteLine(_formatter.Format("Title", Title));
